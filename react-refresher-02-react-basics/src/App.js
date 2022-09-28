c13c12d08931d6e84988a06e129bdcd4de8e5357
JobNieling <jobnieling@gmail.com>
2022-09-28 21:18:48 +0200

    refresher 02-03

---
+++ react-refresher-02-react-basics/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// const App = () => {
+//   return <h1 title="This works!">Hi, <span>this</span> is ReactJS!</h1>;
+// };
+
+// in moderne React gebruiken we alleen functionele components.
+// export  class App extends React.Component {
+//   render() {
+//     return <h1 title='This works'> Hi <span>this</span> is ReactJS!, <span>this</span> changes after every change</h1>;
+//   }
+// }
+
+export default App;
