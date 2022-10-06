72ee4a9c6720ab9d3b93cbcd171a2e6cfebff634
JobNieling <94107702+JobNieling@users.noreply.github.com>
2022-10-06 14:26:19 +0200

    update lesson 13

---
+++ React-Udemy/javascripRefresher/lesson13.js
@@ -0,0 +1,22 @@
+
+// function printMyName(name) {
+//     console.log(name);
+// }
+
+// printMyName('Max');
+
+const printMyName = (name, age) => {
+    console.log(name, age);
+}
+
+printMyName('Kim', 18);
+
+const multiply = (number) => {
+    return number * 2;
+}
+
+console.log(multiply(2));
+
+const singleLineMultiply = number => number * 2;
+
+console.log(singleLineMultiply(8));
