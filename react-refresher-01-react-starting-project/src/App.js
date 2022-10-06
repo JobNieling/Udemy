918baf03c403678fff36f7cd726c8bf5f76acf9b
JobNieling <94107702+JobNieling@users.noreply.github.com>
2022-10-06 13:46:08 +0200

    add map

--- react-refresher-01-react-starting-project/src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// const App = () => { // this is a react component, this can be a function that returns JSX or react create element; or it can be a JS class with extend React.Component { render() { JSX }}
-//   // return <h1>A React App!</h1>;
-//   // return React.createElement('h1', {title: 'This works'}, 'Hi this is React!, this changes after every change');
-//   return <h1 title='This works'> Hi <span>this</span> is ReactJS!, <span>this</span> changes after every change</h1>;
-// };
-
-export default App;
