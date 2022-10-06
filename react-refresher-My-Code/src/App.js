62122e40119e8e7fa7bf11285b5fb57cf4383bd6
JobNieling <94107702+JobNieling@users.noreply.github.com>
2022-10-06 10:21:27 +0200

    update

--- react-refresher-My-Code/src/App.js
+++ react-refresher-My-Code/src/App.js
@@ -11,10 +11,15 @@ const App = () => {
     {id: 'cg3', text: 'Help other students in the Course Q&A'},
   ];
 
+  const addNewGoalHandler = NewGoal => {
+    courseGoals.push(NewGoal);
+    console.log(courseGoals);
+  };
+
   return (
     <div className="course-goals">
       <h2>Course Goals</h2>
-      <NewGoal />
+      <NewGoal onAddGoal={addNewGoalHandler} />
       <GoalList goals={courseGoals} />
     </div>
   );
--- react-refresher-My-Code/src/components/NewGoal/NewGoal.js
+++ react-refresher-My-Code/src/components/NewGoal/NewGoal.js
@@ -2,9 +2,21 @@ import React from "react";
 
 import './NewGoal.css';
 
-const NewGoal = () => {
+const NewGoal = props => {
+
+    const addGoalHandler = event => {
+        event.preventDefault();
+
+        const NewGoal = {
+            id: Math.random().toString(),
+            text: 'My new goal!'
+        };
+
+        props.onAddGoal(NewGoal);
+    };
+
     return ( 
-        <form className="new-goal">
+        <form className="new-goal" onSubmit={addGoalHandler}>
             <input type='text' />
             <button type='submit'>Add Goal</button>
         </form>
